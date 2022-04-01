5622674c96dfd2f6f9054418f56928e85ce2924b
kyslynsky <kyslynsky@gmail.com>
2022-04-01 23:21:41 +0300

    Task 9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,38 @@
+// Обробка відправлення форми form.login-form повинна відбуватися відповідно до події submit.
+// Під час відправлення форми сторінка не повинна перезавантажуватися.
+// Якщо у формі є незаповнені поля, виводь alert з попередженням про те,
+//     що всі поля повинні бути заповнені.
+// Якщо користувач заповнив усі поля і відправив форму,
+//     збери значення полів в об'єкт, де ім'я поля буде ім'ям властивості,
+//     а значення поля - значенням властивості.
+// Для доступу до елементів форми використовуй властивість elements.
+// Виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
 
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Це поле не може бути порожнім");
+  }
+
+  const inputData = {
+    email: email.value,
+    password: password.value,
+  };
+  console.log("inputData", inputData);
+
+  //   const formData = new FormData(event.currentTarget);
+  //   formData.forEach((value, name) => {
+  //     console.log(name, value);
+  //   });
+
+  event.currentTarget.reset();
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
+// Напиши скрипт, який змінює кольори фону елемента < body > через інлайн - стиль
+// по кліку на button.change - color і виводить значення кольору в span.color.
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const body = document.querySelector("body");
+const btnEl = document.querySelector(".change-color");
+const colorNameEl = document.querySelector(".color");
+
+btnEl.addEventListener("click", onBtnClick);
+
+function onBtnClick() {
+  body.style.backgroundColor = `${getRandomHexColor()}`;
+  colorNameEl.textContent = body.style.backgroundColor;
 }
